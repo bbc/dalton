057493a1d96cea18ffaa2833e00a45719f8c6871
Adam Roberts <adam.roberts@bbc.co.uk>
2018-01-13 20:26:00 +0000

    Adding main viewer and styles

--- src/App.js
+++ src/App.js
@@ -1,20 +1,100 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React from 'react';
+import PropTypes from 'prop-types';
+import styles from './styles/css/index.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to Thing</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+const components = {
+  "test": "test",
+  "secondTest": "secondTest"
+};
+
+class Dalton extends React.Component {
+    constructor(props) {
+        super(props);
+        const selected = props.selectedComponent || Object.keys(components)[0];
+        this.state = {
+            selected,
+            Preview: components[selected]
+        };
+
+        this.selectComponent = this.selectComponent.bind(this);
+        this.toggleSidebar = this.toggleSidebar.bind(this);
+    }
+
+    componentDidMount() {
+        window.bbccookies = {
+            _getCurrentDomain: () => window.location.hostname,
+            set: () => null
+        };
+
+        const { selected } = this.state;
+        document.head.insertAdjacentHTML(
+            'beforeend',
+            `<meta name="viewport" content="width=device-width, initial-scale=1">`
+        );
+        document.head.insertAdjacentHTML('beforeend', styles);
+
+        if (!('ontouchstart' in document.documentElement)) {
+            document.documentElement.classList.add('n-no-touchevents');
+        }
+    }
+
+    selectComponent(event) {
+        const selected = event.target.value;
+
+        this.setState({
+            selected,
+            Preview: components[selected]
+        });
+    }
+
+    toggleSidebar() {
+        this.setState({ disabled: !this.state.disabled });
+    }
+
+    render() {
+        const Preview = this.state.Preview;
+
+        return (
+            <div>
+                <div className={this.state.disabled ? 'sidebar__hidden' : 'sidebar'}>
+                    <button className="sidebar__button" onClick={this.toggleSidebar}>
+                        &times;
+                    </button>
+                    <h2 className="sidebar__item">Menu</h2>
+                    {Object.keys(components).map(item => (
+                        <button key={item} onClick={this.selectComponent} value={item} className="sidebar__button">
+                            {item}
+                        </button>
+                    ))}
+                </div>
+                <div className={this.state.disabled ? 'dalton__fullscreen' : 'dalton'}>
+                    <div>
+                        <button
+                            className={this.state.disabled ? 'dalton__button' : 'dalton__button-hidden'}
+                            onClick={this.toggleSidebar}
+                        >
+                            ☰
+                        </button>
+                        <div>
+                            <h2 className="dalton__title">{this.state.selected}</h2>
+                            <hr />
+                            <div className="dalton__container">
+                                {/* <Preview /> */}
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        );
+    }
 }
 
-export default App;
+Dalton.defaultProps = {
+    selectedComponent: ''
+};
+
+Dalton.propTypes = {
+    selectedComponent: PropTypes.string
+};
+
+export default Dalton;
